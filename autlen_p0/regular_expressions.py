9c2754caa7f6045458deaa66e29bd400953046c6
Leandro Garcia <leandro.garcia@estudiante.uam.es>
2021-09-22 20:07:41 +0200

    Posiblemente acabado

--- autlen_p0/regular_expressions.py
+++ autlen_p0/regular_expressions.py
@@ -9,7 +9,7 @@ Completa a continuacion las expresiones regulares para los
 ejercicios 1-5:
 """
 RE1 = "[0-9a-zA-Z_]+.py"
-RE2 = "-?(0|([1-9][0-9]*))?(\.[0-9]*)?"
+RE2 = "-?((0|([1-9][0-9]*))(\.[0-9]*)?|(\.[0-9]+))"
 
 nombreapellidoat = "[a-z]+\.[a-z]+@"
 RE3 = nombreapellidoat + "(estudiante\.)?uam\.es"
@@ -28,6 +28,11 @@ EJERCICIO 6:
 Incluye a continuacion, dentro de esta cadena, tu respuesta 
 al ejercicio 6.
 
-... tu respuesta aqui...
+Como se puede observar, la respuesta dada para los paréntesis anidados recrea la estructura 
+de los mismos manualmente (en la expresión se puede ver cómo se abren dos niveles y luego se cierran).
+Es evidente que esto no se puede aplicar para una profundidad infinita, ya que la expresión nunca terminaría.
+El motivo por el que tampoco es posible recrearlo por otros métodos es que las expresiones regulares carecen
+de un sistema que permita 'memorizar' la profundidad que alcanzamos en cada momento. Esta falta de memoria nos 
+impide saber cuando los paréntesis han sido cerrados correctamente.
 
 """
