e67ea75e15ad992fca713d67a4d6eb9fb87d9bf5
leandroGO <leandroGO@github.com>
2021-09-29 17:46:12 +0200

    Ejercicio 1

--- automata/automaton_evaluator.py
+++ automata/automaton_evaluator.py
@@ -11,13 +11,43 @@ class FiniteAutomatonEvaluator(
     """Evaluator of an automaton."""
 
     def process_symbol(self, symbol: str) -> None:
+        next_states: AbstractSet[_State]
+        next_states = set()
 
-        raise NotImplementedError("This method must be implemented.")
+        if symbol is not None and symbol not in self.automaton.symbols:
+            raise ValueError(
+                f"Symbol {symbol} "
+                f"is not in the set of symbols",
+            )
+
+        for state in self.current_states:
+            for transition in self.automaton.transitions:
+                if state == transition.initial_state and symbol == transition.symbol:
+                    next_states.add(transition.final_state)
+
+        self._complete_lambdas(next_states)
+        self.current_states = next_states
+
 
     def _complete_lambdas(self, set_to_complete: Set[State]) -> None:
+        new_states: AbstractSet[_State]
+        new_states = set()
 
-        raise NotImplementedError("This method must be implemented.")
+        if len(set_to_complete) == 0:
+            return
+
+        for state in set_to_complete:
+            for transition in self.automaton.transitions:
+                if transition.initial_state == state and transition.symbol is None:
+                    new_states.add(transition.final_state)
+        
+        self._complete_lambdas(new_states)
+        set_to_complete.update(new_states)
+
 
     def is_accepting(self) -> bool:
+        for state in self.current_states:
+            if state.is_final:
+                return True
 
-        raise NotImplementedError("This method must be implemented.")
+        return False
