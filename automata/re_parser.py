e611a25d1a05f078f35592877aee5071a0894ffd
Leandro Garcia <leandro.garcia@estudiante.uam.es>
2021-10-02 14:01:20 +0200

    Semana 2 superando tests

--- automata/re_parser.py
+++ automata/re_parser.py
@@ -1,5 +1,5 @@
 """Conversion from regex to automata."""
-from automata.automaton import FiniteAutomaton
+from automata.automaton import FiniteAutomaton, State, Transition
 from automata.re_parser_interfaces import AbstractREParser
 
 
@@ -9,35 +9,154 @@ class REParser(AbstractREParser):
     def _create_automaton_empty(
         self,
     ) -> FiniteAutomaton:
-        raise NotImplementedError("This method must be implemented.")
+        q0: State
+        q0 = State('q0', is_final=True)
+
+        aut: FiniteAutomaton
+        aut = FiniteAutomaton(initial_state=q0, states={q0}, symbols=set(), transitions=set())
+        return aut
+
 
     def _create_automaton_lambda(
         self,
     ) -> FiniteAutomaton:
-        raise NotImplementedError("This method must be implemented.")
+        q0: State
+        q0 = State('q0', is_final=False)
+        qf: State
+        qf = State('qf', is_final=True)
+
+        aut: FiniteAutomaton
+        aut = FiniteAutomaton(initial_state=q0, states={q0, qf}, symbols={None}, transitions={Transition(q0, None, qf)})
+        return aut
 
     def _create_automaton_symbol(
         self,
         symbol: str,
     ) -> FiniteAutomaton:
-        raise NotImplementedError("This method must be implemented.")
+        q0: State
+        q0 = State('q0', is_final=False)
+        qf: State
+        qf = State('qf', is_final=True)
+
+        aut: FiniteAutomaton
+        aut = FiniteAutomaton(initial_state=q0, states={q0, qf}, symbols={None, symbol}, transitions={Transition(q0, symbol, qf)})
+        return aut
 
     def _create_automaton_star(
         self,
         automaton: FiniteAutomaton,
     ) -> FiniteAutomaton:
-        raise NotImplementedError("This method must be implemented.")
+        qf: State
+        qf = State('qf', is_final=True)
+
+        states: Collection[State]
+        states = set()
+        transitions: Collection[Transition]
+        transitions = set()
+        i = 0
+        for state in automaton.states:
+            i += 1
+            state.name = 'q' + str(i)
+            states.add(state)
+            if state.is_final:
+                transitions.add(Transition(state, None, automaton.initial_state))
+                transitions.add(Transition(state, None, qf))
+        states.add(qf)
+
+        for transition in automaton.transitions:
+            transitions.add(transition)
+        transitions.add(Transition(automaton.initial_state, None, qf))
+
+        aut: FiniteAutomaton
+        aut = FiniteAutomaton(initial_state=automaton.initial_state, states=states, symbols=automaton.symbols, transitions=transitions)
+        return aut
 
     def _create_automaton_union(
         self,
         automaton1: FiniteAutomaton,
         automaton2: FiniteAutomaton,
     ) -> FiniteAutomaton:
-        raise NotImplementedError("This method must be implemented.")
+        qf: State
+        qf = State('qf', is_final=True)
+        q0: State
+        q0 = State('q0', is_final=False)
+
+        states: Collection[State]
+        states = set()
+        transitions: Collection[Transition]
+        transitions = set()
+        symbols: Collection[str]
+        symbols = set()
+
+        i = 0
+        for state in automaton1.states:
+            i += 1
+            state.name = 'q' + str(i)
+            states.add(state)
+            if state.is_final:
+                transitions.add(Transition(state, None, qf))
+        for state in automaton2.states:
+            i += 1
+            state.name = 'q' + str(i)
+            states.add(state)
+            if state.is_final:
+                transitions.add(Transition(state, None, qf))
+        states.add(q0)
+        states.add(qf)
+
+        for transition in automaton1.transitions:
+            transitions.add(transition)
+        for transition in automaton2.transitions:
+            transitions.add(transition)
+
+        transitions.add(Transition(q0, None, automaton1.initial_state))
+        transitions.add(Transition(q0, None, automaton2.initial_state))
+
+        for symbol in automaton1.symbols:
+            symbols.add(symbol)
+        for symbol in automaton2.symbols:
+            symbols.add(symbol)
+
+        aut: FiniteAutomaton
+        aut = FiniteAutomaton(initial_state=q0, states=states, symbols=symbols, transitions=transitions)
+        return aut
 
     def _create_automaton_concat(
         self,
         automaton1: FiniteAutomaton,
         automaton2: FiniteAutomaton,
     ) -> FiniteAutomaton:
-        raise NotImplementedError("This method must be implemented.")
+        states: Collection[State]
+        states = set()
+        transitions: Collection[Transition]
+        transitions = set()
+        symbols: Collection[str]
+        symbols = set()
+
+
+        i = 0
+        for state in automaton1.states:
+            i += 1
+            state.name = 'q' + str(i)
+            if state.is_final:
+                transitions.add(Transition(state, None, automaton2.initial_state))
+                state.is_final = False
+            states.add(state)
+        for state in automaton2.states:
+            i += 1
+            state.name = 'q' + str(i)
+            states.add(state)
+
+        for transition in automaton1.transitions:
+            transitions.add(transition)
+        for transition in automaton2.transitions:
+            transitions.add(transition)
+
+        for symbol in automaton1.symbols:
+            symbols.add(symbol)
+        for symbol in automaton2.symbols:
+            symbols.add(symbol)
+
+        aut: FiniteAutomaton
+        aut = FiniteAutomaton(initial_state=automaton1.initial_state, states=states, symbols=symbols, transitions=transitions)
+        return aut
